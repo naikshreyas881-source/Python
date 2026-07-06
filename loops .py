7485912ee5a81855e9413a19210ed197a7e13472
Shreyas Naik <naikshreyas881@gmail.com>
2026-07-06 13:18:51 +0530

    Update loops .py

--- loops .py
+++ loops .py
@@ -1,1 +1,107 @@
+# Print numbers from 1 to 10.
+for i in range (1,11):
+    print(i)
+# output:
+# 1
+# 2
+# 3
+# 4
+# 5
+# 6
+# 7
+# 8
+# 9
+# 10
+
+# Print numbers from 10 to 1.
+for i in range (11 ,0,-1):
+    print(i)
+#     output=11
+# 10
+# 9
+# 8
+# 7
+# 6
+# 5
+# 4
+# 3
+# 2
+# 1
+
+# Print all 0dd numbers from 1 to 20.
+for i in range (1,21,2):
+    print(i)
+# output=1
+# 3
+# 5
+# 7
+# 9
+# 11
+# 13
+# 15
+# 17
+# 19
+
+# Print all even numbers from 1 to 20.
+for i in range (2,21,2):
+    print(i)
+# output=2
+# 4
+# 6
+# 8
+# 10
+# 12
+# 14
+# 16
+# 18
+# 20
+
+# Print the first 15 natural numbers.
+for i in range (1,16):
+    print(i)
+# output=1
+# 2
+# 3
+# 4
+# 5
+# 6
+# 7
+# 8
+# 9
+# 10
+# 11
+# 12
+# 13
+# 14
+# 15
+
+# Print the multiplication table of 7.
+for i in range(1,11):
+    print("7 *",i,"=",7*i)
+# output=7 * 1 = 7
+# 7 * 2 = 14
+# 7 * 3 = 21
+# 7 * 4 = 28
+# 7 * 5 = 35
+# 7 * 6 = 42
+# 7 * 7 = 49
+# 7 * 8 = 56
+# 7 * 9 = 63
+# 7 * 10 = 70
+
+# Find the sum of numbers from 1 to 10
+total = 0
+for i in range (1 ,11):
+    total = total + i
+    print(total)
+# output=1
+# 3
+# 6
+# 10
+# 15
+# 21
+# 28
+# 36
+# 45
+# 55
 
