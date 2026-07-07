a2c2022cf05a70652e52b078a0130387a85e4e2e
Shreyas Naik <naikshreyas881@gmail.com>
2026-07-07 15:26:04 +0530

    Update loops .py

--- loops .py
+++ loops .py
@@ -104,3 +104,16 @@ for i in range (5,0,-1):
 # 123
 # 1234
 # 12345
+
+# Print:
+# 1
+# 12
+# 123
+# 1234
+# 12345
+
+n=5
+for i in range (1,n+1):
+    for j in range(1,i+1):
+        print(j,end="")
+    print()
