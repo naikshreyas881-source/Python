87f8a9006261381fb2b1503c0665df51979bf032
Shreyas Naik <naikshreyas881@gmail.com>
2026-07-13 07:05:47 +0530

    Add colors list and print specific elements
    Added a new list of colors and printed specific elements.

--- lists.py
+++ lists.py
@@ -1,3 +1,8 @@
 list=[1,2,3,4,5,6,7,8,"true,","shreyas",False,4,5,6,7,8,9,546542,6,653,]
 print(list[1:12:2])
 print(list[13])
+
+colors =["red","green","blue","yellow","green"]
+print(colors[-1])
+print(colors[-3])
+print(colors[-5])
