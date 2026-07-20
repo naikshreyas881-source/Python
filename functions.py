cb2c1b29c0b3a59bbd553a0e03ff145eb8b7d231
Shreyas Naik <naikshreyas881@gmail.com>
2026-07-20 20:02:37 +0530

    Update functions.py

--- functions.py
+++ functions.py
@@ -129,6 +129,17 @@ result=add(4,5)
 print(result)
 
 
+nums=[5,6,7,7,1,9,111,1,1,5,1,1]
+freq_map = {}
+
+for i in range(len(nums)):
+    if nums[i] in freq_map:
+        freq_map[nums[i]] += 1
+    else:
+        freq_map[nums[i]] = 1
+
+print(freq_map)
+
 
 
 
