4d4c3a1d07af4801ce1e6974c72c52067298956c
Shreyas Naik <naikshreyas881@gmail.com>
2026-07-29 21:42:36 +0530

    Update loops .py

--- loops .py
+++ loops .py
@@ -131,3 +131,13 @@ while num>0:
     num=num//10
 print(num%10)
 # output= 5 8 7 0
+
+#Count the number of Digits in an Integer
+ num=75443467
+ count=0
+
+ while num>0:
+     count+=1
+     num=num//10
+    print(count)
+#output=8
