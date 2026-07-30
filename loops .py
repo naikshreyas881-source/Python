44ff8dfe659468735b534b26b32f308cae4da4a2
Shreyas Naik <naikshreyas881@gmail.com>
2026-07-30 21:02:12 +0530

    Implement palindrome check in loops.py
    Added a function to check if a number is a palindrome.

--- loops .py
+++ loops .py
@@ -141,3 +141,19 @@ print(num%10)
      num=num//10
     print(count)
 #output=8
+
+Check if a number is Palindrome or Not
+num = 153
+n = num
+rev = 0
+
+while num > 0:
+    ld = num % 10
+    rev = rev * 10 + ld
+    num = num // 10
+
+if rev == n:
+    print("Palindrome")
+else:
+    print("Not a Palindrome")
+    #output= not a palindrome 
