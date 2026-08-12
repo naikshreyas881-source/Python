c8588b7c26d843cd02604870d74fe4f1f0a3f9a7
Shreyas Naik <naikshreyas881@gmail.com>
2026-08-12 13:34:08 +0530

    Update conditional statements.py

--- conditional statements.py
+++ conditional statements.py
@@ -57,4 +57,20 @@ class Solution:
 
         return func(0, len(s) - 1)
 
+#check it is leaf year or not 
+def is_leap(year):
+    leap = False
+    if year % 400 == 0:
+        return True
+    elif year % 100 == 0:
+        return False
+    elif year % 4 == 0:
+        return True
+    else:
+        return False
+    # Write your logic here
+    
+    return leap
+year = int(input())
+
 
