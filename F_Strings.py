d5e84ff6a4073d258acd1bbc1d053ebcd27b9c77
Shreyas Naik <naikshreyas881@gmail.com>
2026-09-18 21:10:05 +0530

    Update F_Strings.py

--- F_Strings.py
+++ F_Strings.py
@@ -2,3 +2,16 @@ letter="hey my name is{}and i am from {}"
 name="shreyas"
 country="india"
 print(letter.format(name,country))
+
+
+#Convert Seconds into Hours, Minutes, and Seconds
+n=7384
+hour=(7384/(60*24))
+minutes=(7384/60)
+seconds=n
+print(hour)
+print(minutes)
+print(seconds)
+# output=5.127777777777778
+# 123.06666666666666
+# 7384
