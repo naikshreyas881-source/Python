ff0081ee8a4a01ec9307b9a4a5d70bd809b89b8f
Shreyas Naik <naikshreyas881@gmail.com>
2026-09-23 21:27:53 +0530

    Update Break and continue statements .py

--- Break and continue statements .py
+++ Break and continue statements .py
@@ -5,3 +5,11 @@ for i in range (1,101,1):
 else:
    print("task complited")
    print("Done with that task")
+
+#Calculate Compound Interest
+p=10000
+r=10
+t=2
+compound_intrest=p*((1+r/100)*(1+r/100))
+print(compound_intrest)
+# output=12100.000000000002
